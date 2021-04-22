28a982d6d148c8c6c70bf4e7ba63d87be4cd3738
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-04-22 07:53:05 +0000

    リレーションの定義

--- app/ChildHelp.php
+++ app/ChildHelp.php
@@ -3,9 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ChildHelp extends Model
+class ChildHelp extends Pivot
 {
     //
     protected $table = 'child_help'; // tableを指定
+    
+    public function child()
+    {
+        return $this->belongsTo('App\Child');
+    }
+    
+    public function help()
+    {
+        return $this->belongsTo('App\Help');
+    }
 }
