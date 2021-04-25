640ecb27ecf2debae36dcd6af8761a060bd1efca
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-04-25 23:53:08 +0000

    seederに未就学児と成人を追加

--- database/seeds/SetsTableSeeder.php
+++ database/seeds/SetsTableSeeder.php
@@ -14,18 +14,20 @@ class SetsTableSeeder extends Seeder
     {
         //
         $first_sets = [
-            ['base_year' => 1, 'grade' => '小学１年', 'magnification' => 1.0],
-            ['base_year' => 2, 'grade' => '小学２年', 'magnification' => 2.0],
-            ['base_year' => 3, 'grade' => '小学３年', 'magnification' => 3.0],
-            ['base_year' => 4, 'grade' => '小学４年', 'magnification' => 4.0],
-            ['base_year' => 5, 'grade' => '小学５年', 'magnification' => 5.0],
-            ['base_year' => 6, 'grade' => '小学６年', 'magnification' => 6.0],
-            ['base_year' => 7, 'grade' => '中学１年', 'magnification' => 10.0],
-            ['base_year' => 8, 'grade' => '中学２年', 'magnification' => 12.0],
-            ['base_year' => 9, 'grade' => '中学３年', 'magnification' => 15.0],
-            ['base_year' => 10, 'grade' => '高校１年', 'magnification' => 30.0],
-            ['base_year' => 11, 'grade' => '高校２年', 'magnification' => 40.0],
-            ['base_year' => 12, 'grade' => '高校３年', 'magnification' => 50.0],
+            ['base_year' => 5, 'grade' => '未就学児', 'magnification' => 0],
+            ['base_year' => 6, 'grade' => '小学１年', 'magnification' => 1.0],
+            ['base_year' => 7, 'grade' => '小学２年', 'magnification' => 2.0],
+            ['base_year' => 8, 'grade' => '小学３年', 'magnification' => 3.0],
+            ['base_year' => 9, 'grade' => '小学４年', 'magnification' => 4.0],
+            ['base_year' => 10, 'grade' => '小学５年', 'magnification' => 5.0],
+            ['base_year' => 11, 'grade' => '小学６年', 'magnification' => 6.0],
+            ['base_year' => 12, 'grade' => '中学１年', 'magnification' => 10.0],
+            ['base_year' => 13, 'grade' => '中学２年', 'magnification' => 12.0],
+            ['base_year' => 14, 'grade' => '中学３年', 'magnification' => 15.0],
+            ['base_year' => 15, 'grade' => '高校１年', 'magnification' => 30.0],
+            ['base_year' => 16, 'grade' => '高校２年', 'magnification' => 40.0],
+            ['base_year' => 17, 'grade' => '高校３年', 'magnification' => 50.0],
+            ['base_year' => 18, 'grade' => '成人', 'magnification' => 0],
         ];
         
         foreach($first_sets as $first_set){
