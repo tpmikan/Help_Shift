800c6936b4428c52b84afaaa7bedf8b951707cea
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-05-01 01:34:35 +0000

    機能の変更

--- resources/views/parent/home.blade.php
+++ resources/views/parent/home.blade.php
@@ -43,10 +43,7 @@
       </div>
       <div class="row">
         <div class="col-3 px-1 pb-1">
-          <a href="" role="button" class="btn btn-primary btn-block">お小遣い明細 作成</a>
-        </div>
-        <div class="col-3 px-1 pb-1">
-          <a href="" role="button" class="btn btn-primary btn-block">お小遣い明細 確認</a>
+          <a href="{{ action('Admin\ParentController@showCalculation') }}" role="button" class="btn btn-primary btn-block">お小遣い 計算</a>
         </div>
         <div class="col-3 px-1 pb-1">
           <a href="{{ action('Admin\ParentController@showSet') }}" role="button" class="btn btn-primary btn-block">設定</a>
