269a10236a7ae54d507d2395de317c9497d88be2
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-05-07 23:52:38 +0000

    コードの修正

--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -32,11 +32,11 @@
               <!-- Authentication Links -->
               @guest
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link" href="{{ route('parent.login') }}">{{ __('Login') }}</a>
                 </li>
-                @if (Route::has('register'))
+                @if (Route::has('parent.register'))
                   <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    <a class="nav-link" href="{{ route('parent.register') }}">{{ __('Register') }}</a>
                   </li>
                 @endif
                 @else
@@ -46,11 +46,11 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                      <a class="dropdown-item" href="{{ route('parent.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
                       </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    <form id="logout-form" action="{{ route('parent.logout') }}" method="POST" style="display: none;">
                       @csrf
                     </form>
                   </div>
