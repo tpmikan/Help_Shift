86da54886bc5310bb83be81d798b266c9d049a1d
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-05-08 02:31:00 +0000

    デバッグ

--- resources/views/parent/home.blade.php
+++ resources/views/parent/home.blade.php
@@ -6,24 +6,24 @@
 
     <h1>Parent Home</h1>
     <!-- カレンダー-->
-    <div class="container">
-      <div class="row align-items-end">
-        <div class="col-2">
-          <button id="previous" class="btn btn-success btn-block">＜　前月</button>
-        </div>
-        <div class="col-3">
-          <h4 id="yearAndMonth"></h4>
-        </div>
-        <div class="col-2">
-  　  　    <button id="next" class="btn btn-success btn-block">次月　＞</button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-7 mb-5">
-    　  　  <div id="calendar"></div>
-        </div>
-      </div>
-    </div>
+    <!--<div class="container">-->
+    <!--  <div class="row align-items-end">-->
+    <!--    <div class="col-2">-->
+    <!--      <button id="previous" class="btn btn-success btn-block">＜　前月</button>-->
+    <!--    </div>-->
+    <!--    <div class="col-3">-->
+    <!--      <h4 id="yearAndMonth"></h4>-->
+    <!--    </div>-->
+    <!--    <div class="col-2">-->
+  　 <!-- 　    <button id="next" class="btn btn-success btn-block">次月　＞</button>-->
+    <!--    </div>-->
+    <!--  </div>-->
+    <!--  <div class="row">-->
+    <!--    <div class="col-7 mb-5">-->
+    <!--　  　  <div id="calendar"></div>-->
+    <!--    </div>-->
+    <!--  </div>-->
+    <!--</div>-->
     
     
     <div class="container-fluid">
