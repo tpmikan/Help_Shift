96f3a18353beb91925707ec7076a5172c206634f
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-06-05 02:18:28 +0000

    コメントの追加

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,16 +15,18 @@ class Controller extends BaseController
     public function gradeCalculation($day){
       $today = date("Ymd");
       $birthday = date('Ymd',strtotime($day));
-      $base_day = date('Y',strtotime($birthday)) . date('md',strtotime('0000-04-02'));
-      $date = $base_day - $birthday; 
+      $base_day = date('Y',strtotime($birthday)) . date('md',strtotime('0000-04-02')); //学年の境目を指定
+      $date = $base_day - $birthday;
       $criteria = 0;
       
+      //早生まれかどうかの計算
       if ($date > 0){
         $criteria = date('Y',strtotime($birthday)) . date('md',strtotime('0000-04-01'));
       } elseif ($date <= 0) {
         $criteria = date('Y',strtotime($birthday. "+1 year")) . date('md',strtotime('0000-04-01'));
       }
       
+      //入学年度の計算
       $base_year = number_format(floor(($today - $criteria)/10000));
       
       if ($base_year <= 5){
